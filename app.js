6b3c77cd750a9f773a72962ab9db498d3bca3488
menachem95 <menachemf95@gmail.com>
2023-09-10 21:57:26 +0300

    cors

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.options('*', cors());
+app.options('*', cors());
 
 app.use(cors({
   origin: "http://localhost:3000", // Vervang door de juiste oorsprong
