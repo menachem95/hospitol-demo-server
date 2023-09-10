70aca18e24689c5938fb2b15f879e531ed143421
menachem95 <menachemf95@gmail.com>
2023-09-10 22:00:42 +0300

    cors test

--- app.js
+++ app.js
@@ -27,13 +27,13 @@ app.use((req, res, next) => {
 
 app.options('*', cors());
 
-app.use(cors({
-  origin: "http://localhost:3000", // Vervang door de juiste oorsprong
-  methods: "GET, POST, PUT, DELETE, OPTIONS", // Vervang door de toegestane methoden
-  allowedHeaders: "Content-Type", // Vervang door de toegestane headers
-  credentials: true, // Schakel cookies en verificatie in indien nodig
-  optionsSuccessStatus: 200
-}));
+// app.use(cors({
+//   origin: "http://localhost:3000", // Vervang door de juiste oorsprong
+//   methods: "GET, POST, PUT, DELETE, OPTIONS", // Vervang door de toegestane methoden
+//   allowedHeaders: "Content-Type", // Vervang door de toegestane headers
+//   credentials: true, // Schakel cookies en verificatie in indien nodig
+//   optionsSuccessStatus: 200
+// }));
 
 app.use(bodyParser.json());
 
